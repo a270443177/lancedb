778fc4ac24d8d1ed6305219233bd92f3e078612d
a270443177 <chenwenbin1991@126.com>
2025-03-07 12:29:02 +0800

    Update build_lancedb.sh

--- ci/manylinux_node/build_lancedb.sh
+++ ci/manylinux_node/build_lancedb.sh
@@ -13,7 +13,7 @@ export OPENSSL_INCLUDE_DIR=/usr/local/include/openssl
 
 #Alpine doesn't have .bashrc
 FILE=$HOME/.bashrc && test -f $FILE && source $FILE
-
+export PATH="$PATH:$HOME/.nvm/node-v18.20.7-linux-x64-glibc-217/bin"
 cd nodejs
 npm ci
 npm run build-release
